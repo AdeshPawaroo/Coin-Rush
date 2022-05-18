08a6e37a066c164ee06639d87a2167d45c74c01b
adesh <adeshpawaroo1@gmail.com>
2022-05-18 00:18:45 -0400

    fix error for getting canvas

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,6 +1,6 @@
 export default class Canvas {
     constructor() {
-        this.canvas = document.getElementsByClassName("game-canvas");
+        this.canvas = document.getElementById("game-canvas");
         this.ctx = this.canvas.getContext("2d");
     }
 }
