23c4e8ff51a76a97c7b021cec1acb3d2ae2c7a18
adesh <adeshpawaroo1@gmail.com>
2022-05-27 04:02:27 -0400

    scaled coin spawnpoints / fixing hit box bug

--- src/scripts/coin.js
+++ src/scripts/coin.js
@@ -1,7 +1,7 @@
 export default class Coin {
     constructor() {
         this.x = 520;
-        this.y = 52;
+        this.y = 272;
         this.fX = 0;
         this.fY = 0;
         this.width = 16;
@@ -12,6 +12,18 @@ export default class Coin {
             [520, 52],
             [740, 52],
             [980, 52],
+
+            [70, 272],
+            [320, 272],
+            [520, 272],
+            [740, 272],
+            [980, 272],
+
+            [70, 520],
+            [320, 520],
+            [520, 520],
+            [740, 520],
+            [980, 520],
         ];
     }
 
@@ -28,14 +40,12 @@ export default class Coin {
     }
     
     collected(player) {
-        if ((player.x === this.x || player.x - this.x === -20 || player.x - this.x === 20 || player.x - this.x === 10 || player.x - this.x === -10) &&
-            (player.y === this.y || player.y - this.y === -20 || player.y - this.y === 20 || player.y - this.y === -40)
-        ) {
+        if (player.x === this.x || player.x - this.x === -20 || player.x - this.x === 20 || player.x - this.x === 10 || player.x - this.x === -10) {
             this.generatePos();
         }
-        // if (player.y === this.y || player.y - this.y === -20 || player.y - this.y === 20 || player.y - this.y === -40) {
-        //     this.generatePos();
-        // }
+        if (player.y === this.y || player.y - this.y === -20 || player.y - this.y === 20 || player.y - this.y === -40) {
+                this.generatePos();
+        }
     }
 
     spinningAnimation() {
